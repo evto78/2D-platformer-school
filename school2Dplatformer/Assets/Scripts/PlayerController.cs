ab6ce78bfa5bfae1676c2485908532dbe91fee7b
evto78 <150751428+evto78@users.noreply.github.com>
2024-11-27 15:15:26 -0500

    Task 1 2 and 3 complete

--- school2Dplatformer/Assets/Scripts/PlayerController.cs
+++ school2Dplatformer/Assets/Scripts/PlayerController.cs
@@ -2,43 +2,85 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float maxSpeed;
+    public float speed;
+
+    Rigidbody2D rb;
+
+    
+
     public enum FacingDirection
     {
         left, right
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // The input from the player needs to be determined and
-        // then passed in the to the MovementUpdate which should
-        // manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+
+        Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        if(rb.velocity.x < 0f && playerInput.x > 0f) { rb.velocity = rb.velocity * Vector2.left; }
+        if(rb.velocity.x > 0f && playerInput.x < 0f) { rb.velocity = rb.velocity * Vector2.left; }
 
+        rb.AddForce(new Vector2(playerInput.x, 0f) * speed * 100f * Time.deltaTime);
+
+        if (rb.velocity.x > maxSpeed)
+        {
+            rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+        }
+        else if (rb.velocity.x < -maxSpeed)
+        {
+            rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+        }
     }
 
     public bool IsWalking()
     {
-        return false;
+        if(rb.velocity.x == 0f)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+        
     }
     public bool IsGrounded()
     {
-        return false;
+        if(Physics2D.BoxCast(new Vector2(transform.position.x, transform.position.y), new Vector2(0.33f, 1f), 0f, Vector2.down))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+        if(rb.velocity.x > 0f)
+        {
+            return FacingDirection.right;
+        }
+        else if (rb.velocity.x < 0f)
+        {
+            return FacingDirection.left;
+        }
+        else
+        {
+            return FacingDirection.right;
+        }
+        
     }
 }
